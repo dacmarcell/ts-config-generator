014b9f6dc69db2eebbc3d61813510108b9d2f699
marcelldac <dacmarcell@gmail.com>
2024-05-18 13:12:10 -0300

    feat: Refactor tsconfig generation to use templates

--- src/main.rs
+++ src/main.rs
@@ -1,173 +1,49 @@
-use std::io::Write;
-use std::{error::Error, fs::File};
+use std::error::Error;
 use console::{style, Style};
 use dialoguer::{theme::ColorfulTheme, Select};
 
-#[derive(Debug)]
-#[allow(dead_code)]
-struct Config {
-    template: usize,
-}
+use config::Config;
+use templates::{generate_tsconfig, TEMPLATE_NAMES};
 
-fn generate_tsconfig(template: usize) -> std::io::Result<()> {
-    let mut file = File::create("tsconfig.json")?;
-    
-    match template {
-        0 => { //Recommended
-            let content = "{ 
-  \"compilerOptions\": {
-    \"target\": \"es2016\",
-    \"module\": \"commonjs\",
-    \"esModuleInterop\": true,
-    \"forceConsistentCasingInFileNames\": true,
-    \"strict\": true,
-    \"skipLibCheck\": true
-  },
-  \"$schema\": \"https://json.schemastore.org/tsconfig\"
-}";
-            file.write_all(content.as_bytes())?;
-        },
-        1 => { //Node 21
-            let content = "{
-  \"$schema\": \"https://json.schemastore.org/tsconfig\",
-  \"_version\": \"21.0.0\",
-            
-  \"compilerOptions\": {
-    \"lib\": [\"es2023\"],
-    \"module\": \"node16\",
-    \"target\": \"es2022\",
-              
-    \"strict\": true,
-    \"esModuleInterop\": true,
-    \"skipLibCheck\": true,
-    \"moduleResolution\": \"node16\"
-  }
-}";
-            file.write_all(content.as_bytes())?;
-        },
-        2 => {//Node 20
-            let content = "{
-  \"$schema\": \"https://json.schemastore.org/tsconfig\",
-  \"_version\": \"20.1.0\",
-              
-  \"compilerOptions\": {
-    \"lib\": [\"es2023\"],
-    \"module\": \"node16\",
-    \"target\": \"es2022\",
-             
-    \"strict\": true,
-    \"esModuleInterop\": true,
-    \"skipLibCheck\": true,
-    \"moduleResolution\": \"node16\"
-}
-}";
-            file.write_all(content.as_bytes())?;
-        },
-        3 => {//Node 19
-            let content = "{
-  \"$schema\": \"https://json.schemastore.org/tsconfig\",
-              
-  \"_version\": \"19.1.0\",
-              
-  \"compilerOptions\": {
-    \"lib\": [\"es2023\"],
-    \"module\": \"node16\",
-    \"target\": \"es2022\",
-              
-    \"strict\": true,
-    \"esModuleInterop\": true,
-    \"skipLibCheck\": true,
-    \"moduleResolution\": \"node16\"
-    }
-}";
-            file.write_all(content.as_bytes())?;
-        },
-        4 => {//Bun
-            let content = "{
-  // This is based on https://bun.sh/docs/typescript#suggested-compileroptions
-  \"$schema\": \"https://json.schemastore.org/tsconfig\",
-  \"docs\": \"https://bun.sh/docs/typescript\",
-
-  \"compilerOptions\": {
-    // Enable latest features
-    \"lib\": [\"ESNext\"],
-    \"target\": \"ESNext\",
-    \"module\": \"ESNext\",
-    \"moduleDetection\": \"force\",
-    \"jsx\": \"react-jsx\",
-    \"allowJs\": true,
-
-    // Bundler mode
-    \"moduleResolution\": \"bundler\",
-    \"allowImportingTsExtensions\": true,
-    \"verbatimModuleSyntax\": true,
-    \"noEmit\": true,
-
-    // Best practices
-    \"strict\": true,
-    \"skipLibCheck\": true,
-    \"noFallthroughCasesInSwitch\": true,
-
-    // Some stricter flags
-    \"noUnusedLocals\": true,
-    \"noUnusedParameters\": true,
-    \"noPropertyAccessFromIndexSignature\": true
-}
-              }";
-            file.write_all(content.as_bytes())?;
-        },
-        _ => {
-            panic!("Invalid template")
-        }   
-    }
-
-    Ok(())
-}
+mod config;
+mod templates;
 
 fn init_config() -> Result<Option<Config>, Box<dyn Error>> {
-    let theme = ColorfulTheme {
-        values_style: Style::new().yellow().dim(),
-        ..ColorfulTheme::default()
-    };
+	let theme = ColorfulTheme {
+		values_style: Style::new().yellow().dim(),
+		..ColorfulTheme::default()
+	};
 
-    let ascii_art = r#"
-   $$$$$$$$\ $$$$$$\          $$\                                   $$\          $$\                       
-   \__$$  __$$  __$$\         $$ |                                  $$ |         $$ |                      
-      $$ |  $$ /  \__|      $$$$$$\   $$$$$$\ $$$$$$\$$$$\  $$$$$$\ $$ |$$$$$$\$$$$$$\   $$$$$$\  $$$$$$$\ 
-      $$ |  \$$$$$$\        \_$$  _| $$  __$$\$$  _$$  _$$\$$  __$$\$$ |\____$$\_$$  _| $$  __$$\$$  _____|
-      $$ |   \____$$\         $$ |   $$$$$$$$ $$ / $$ / $$ $$ /  $$ $$ |$$$$$$$ |$$ |   $$$$$$$$ \$$$$$$\  
-      $$ |  $$\   $$ |        $$ |$$\$$   ____$$ | $$ | $$ $$ |  $$ $$ $$  __$$ |$$ |$$\$$   ____|\____$$\ 
-      $$ |  \$$$$$$  |        \$$$$  \$$$$$$$\$$ | $$ | $$ $$$$$$$  $$ \$$$$$$$ |\$$$$  \$$$$$$$\$$$$$$$  |
-      \__|   \______/          \____/ \_______\__| \__| \__$$  ____/\__|\_______| \____/ \_______\_______/ 
-                                                           $$ |                                            
-                                                           \__|                                            
-"#;
+	let ascii_art = r#"
+			$$$$$$$$\ $$$$$$\          $$\                                   $$\          $$\                       
+			\__$$  __$$  __$$\         $$ |                                  $$ |         $$ |                      
+				$$ |  $$ /  \__|      $$$$$$\   $$$$$$\ $$$$$$\$$$$\  $$$$$$\ $$ |$$$$$$\$$$$$$\   $$$$$$\  $$$$$$$\ 
+				$$ |  \$$$$$$\        \_$$  _| $$  __$$\$$  _$$  _$$\$$  __$$\$$ |\____$$\_$$  _| $$  __$$\$$  _____|
+				$$ |   \____$$\         $$ |   $$$$$$$$ $$ / $$ / $$ $$ /  $$ $$ |$$$$$$$ |$$ |   $$$$$$$$ \$$$$$$\  
+				$$ |  $$\   $$ |        $$ |$$\$$   ____$$ | $$ | $$ $$ |  $$ $$ $$  __$$ |$$ |$$\$$   ____|\____$$\ 
+				$$ |  \$$$$$$  |        \$$$$  \$$$$$$$\$$ | $$ | $$ $$$$$$$  $$ \$$$$$$$ |\$$$$  \$$$$$$$\$$$$$$$  |
+				\__|   \______/          \____/ \_______\__| \__| \__$$  ____/\__|\_______| \____/ \_______\_______/ 
+																														$$ |                                            
+																														\__|                                            
+	"#;
 
+	println!("{}", style(ascii_art).blue());
 
-    println!("{}", style(ascii_art).blue());
+	let template = Select::with_theme(&theme)
+		.with_prompt("Choose a template")
+		.default(0)
+		.items(&TEMPLATE_NAMES)
+		.interact()?;
 
-    //TODO: implements a dinamic template version
-    let template = Select::with_theme(&theme)
-        .with_prompt("Choose a template")
-        .default(0)
-        .item("Recommended")
-        .item("Node 21")
-        .item("Node 20")
-        .item("Node 19")
-        .item("Bun")
-        .interact()?;
+	generate_tsconfig(template)?;
 
-    generate_tsconfig(template)?;
-
-    Ok(Some(Config {
-        template
-    }))
+	Ok(Some(Config { template }))
 }
 
 fn main() {
-    match init_config() {
-        Ok(None) => println!("Aborted."),
-        Ok(Some(_)) => println!("Generated {} file in root path", style("tsconfig.json").blue()),
-        Err(err) => println!("error: {}", err),
-    }
+	match init_config() {
+		Ok(None) => println!("Aborted."),
+		Ok(Some(_)) => println!("Generated {} file in root path", style("tsconfig.json").blue()),
+		Err(err) => println!("error: {}", err),
+	}
 }
